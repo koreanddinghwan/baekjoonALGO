13b57faa0d65182046ca434479ea801272428531
myunghwanKang <myunghwan0421@gmail.com>
2021-10-14 14:44:26 +0900

    Update 9#1929.py

--- step9mathematics2/9#1929.py
+++ step9mathematics2/9#1929.py
@@ -22,3 +22,30 @@ def isprime(a):
 for i in range(m,n+1):
     if isprime(i):
         print(i)
+
+
+def prime_list(m,n):
+    # 에라토스테네스의 체 초기화: n+1개 요소에 True 설정(소수로 간주),인덱스넘버=숫자
+    sieve = [True] * (n+1)  
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    checkerrange = int((n+1) ** 0.5)
+    for i in range(2, checkerrange + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n+1, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+    sieve[0] = False
+    sieve[1] = False
+
+    # 소수 목록 반환
+    return sieve
+
+
+import sys
+
+m,n = map(int, sys.stdin.readline().split())
+
+sieve_list = prime_list(m,n)
+for index, value in enumerate(sieve_list):
+    if index >= m and value == True:
+        print(index)
