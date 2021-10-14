49e4bd7709a30e3793f8d090ddf83d5afc5c7b83
myunghwanKang <myunghwan0421@gmail.com>
2021-10-14 11:41:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
-import sys
-
-n = int(input())
-print(True*n)
+class stack:
+    def __init__(self):
+        self.items = []
+        
+    def push(self, val):
+        self.items.append(val)
+        
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            print("empty stack")
+            
+    def top(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            print("empty stack")
+            
+    def __len__(self):
+        return len(self.items)
+    
+    def isEmpty(self):
+        return len(self) == 0
