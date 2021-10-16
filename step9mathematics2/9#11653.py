f41fa441cda2060214f4ec52793916df0b36795e
myunghwanKang <myunghwan0421@gmail.com>
2021-10-16 13:24:20 +0900

    Update 9#11653.py

--- step9mathematics2/9#11653.py
+++ step9mathematics2/9#11653.py
@@ -1,15 +1,34 @@
+
+def prime_list(a):
+    sieve = [True] * (a+1)
+    sosu_list = []
+    checkrange = int((a+1)**0.5)
+
+    for i in range(2,checkrange+1):
+        if sieve[i] == True:
+            for j in range(i+i,a+1,i):
+                sieve[j] = False
+
+    sieve[0] = False
+    sieve[1] = False
+
+    
+    for index,value in enumerate(sieve):
+        if value == True:
+            sosu_list.append(index)
+
+    return sosu_list
+
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-a = n
-k = 2
-while  a != 1:
-    if a % k == 0:
-        print(k)
-        a = a / k
-        continue
-    else:
-        k = k + 1
 
-    
 
+
+
+for s in prime_list(n):
+    if n == 1:
+        break
+    while n % s == 0:
+        n = n // s
+        print(s)
