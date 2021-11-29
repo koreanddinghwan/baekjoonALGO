5c041a0d90e4235544bc2237aa5de26f15ac9f2c
koreanddinghwan <myunghwan0421@gmail.com>
2021-11-29 22:50:19 +0900

    st

---
+++ notnumbered/9093.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+
+for i in range(n):
+    text = sys.stdin.readline().split()
+    
+    for char in text:
+        
+
