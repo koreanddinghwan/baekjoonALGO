10e67ad02966208f8976e82973e2e1c960d3b07c
koreanddinghwan <myunghwan0421@gmail.com>
2021-12-02 03:12:22 +0900

    Create 17299.py

---
+++ notnumbered/17299.py
@@ -0,0 +1,27 @@
+import sys
+
+from collections import Counter
+
+n = int(sys.stdin.readline().rstrip())
+
+lst = list(map(int, sys.stdin.readline().split()))
+
+#정답을 저장하기위한 리스트
+ngf = [-1 for _ in range(n)]
+
+#스택,
+stack = [0]
+
+count = dict(Counter(lst))
+
+for i in range(1,n):
+    #스택에 값이 들어있고, 스택의 마지막값의 카운트가 현재 값의 카운트보다작다면
+    while stack and count[lst[stack[-1]]] < count[lst[i]]:
+        #스택을 풀고 오등큰수를 부여
+        ngf[stack.pop()] = lst[i]
+
+    #while문에서 빠져나왔다는건, 스택에 더이상 오등큰수를 부여할 수가 없다는것.
+    #다음 i가 검증하도록 stack에 i부여
+    stack.append(i)
+
+print(" ".join(map(str, ngf)))
